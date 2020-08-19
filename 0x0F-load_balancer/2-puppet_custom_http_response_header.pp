96d5fbc8f80a693404ad962cd99aa6437e911bb0
Myduzo <sahli.youssef@outlook.com>
2020-08-19 16:04:55 +0100

    load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 # automate the task of creating a custom HTTP header response with Puppet.
-exec { 
-  command => "sudo apt-get -y update;
-              sudo apt-get -y install nginx;
-              echo "\Holberton School"\ > /var/www/html/index.nginx-debian.html;
-              echo "\Ceci n'est pas une page"\ > /var/www/html/404.html;
-              printf %s "\server {
-                  listen 80;
-                  listen [::]:80 default_server;
-                  root   /usr/share/nginx/html;
-                  index  index.html index.htm;
-                  location /redirect_me {
-                      return 301 http://youtube.com/watch?v=QH2-TGUlwu4;
-                  }
-                  error_page 404 /404.html;
-                  location /404 {
-                    root /usr/share/nginx/html;
-                    internal;
-                  }
-                  add_header X-Served-By $HOSTNAME;
-              }"\ > /etc/nginx/sites-available/default;
-              service nginx restart"
-}
+exec {
+  command => 'sudo apt-get -y update;
+sudo apt-get -y install nginx;
+
+echo "Holberton School" > /var/www/html/index.nginx-debian.html;
+echo "Ceci n'est pas une page" > /var/www/html/404.html;
+printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   /usr/share/nginx/html;
+    index  index.html index.htm;
+    location /redirect_me {
+        return 301 http://youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /usr/share/nginx/html;
+      internal;
+    }
+    add_header X-Served-By $HOSTNAME;
+}" > /etc/nginx/sites-available/default;
+service nginx restart',
+    provider => shell,
+      }
