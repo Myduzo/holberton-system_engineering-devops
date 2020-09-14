04d5ea2f8f92f9a82418611b30e41ac4212339da
Myduzo <sahli.youssef@outlook.com>
2020-09-14 17:04:19 +0100

    todo list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """script that returns information about his/her TODO list progress."""
 
-import json
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    EMPLOYEE_NAME = requests.get('https://jsonplaceholder.typicode.com/users/{}\
-        '.format(argv[1])).json().get('name')
-    req_todo = requests.get('https://jsonplaceholder.typicode.com/todos\
-        ').json()
+    req = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                       .format(argv[1]))
+    EMPLOYEE_NAME = req.json().get('name')
+    req2 = requests.get('https://jsonplaceholder.typicode.com/todos')
+    req_todo = req2.json()
     my_list = []
     TOTAL_NUMBER_OF_TASKS = 0
     for task in req_todo:
@@ -18,7 +18,7 @@ if __name__ == "__main__":
             TOTAL_NUMBER_OF_TASKS += 1
             if str(task.get('completed')) == 'True':
                 my_list.append(task.get('title'))
-    print('Employee {} is done with tasks({}/{}):\
-        '.format(EMPLOYEE_NAME, len(my_list), TOTAL_NUMBER_OF_TASKS))
+    print('Employee {} is done with tasks({}/{}):'
+          .format(EMPLOYEE_NAME, len(my_list), TOTAL_NUMBER_OF_TASKS))
     for index in my_list:
         print('\t {}'.format(index))
