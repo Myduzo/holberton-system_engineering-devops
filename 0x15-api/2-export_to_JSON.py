8c334245afb135f561d7e2a389165c36a78fbf10
Myduzo <sahli.youssef@outlook.com>
2020-09-15 15:48:52 +0100

    export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Using what we did in the task #0, extend Python script to export data in the JSON format."""
+"""Using what we did in the task #0
+extend Python script to export data in the JSON format.
+"""
 
 import json
 import requests
@@ -7,17 +9,19 @@ from sys import argv
 
 
 if __name__ == "__main__":
-	my_list = []
-	d = {}
-	d1 = {}
-	EMPLOYEE_NAME = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
-	req_todo = requests.get('https://jsonplaceholder.typicode.com/todos')
-	li = req_todo.json()
-	for dec in li:
-		for k, v in dec.items():
-			if k == "userId" and v == int(argv[1]):
-				d = {"task": dec.get("title"), "completed": dec.get("completed"), "username": dec.get("username")}
-				my_list.append(d)
-			d1 = {argv[1]: my_list}
-	with open('{}.json'.format(argv[1]), 'w+') as file:
-		file.write(str(d1))
+    req_name = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                            .format(argv[1]))
+    name = req_name.json().get("username")
+
+    req_todo = requests.get("http://jsonplaceholder.typicode.com/" +
+                            "users/{}/todos".format(argv[1]))
+    todo = req_todo.json()
+    
+    with open("{}.json".format(argv[1]), "w") as jsonfile:
+        out = [{
+                "task":         task.get('title'),
+                "completed":    task.get('completed'),
+                "username":     name
+               } for task in todo]
+
+        json.dump({"{}".format(argv[1]): out}, jsonfile)
