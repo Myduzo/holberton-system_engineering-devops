372bbeb673bfed80d236c86e9a0d7da6fe5a1143
Myduzo <sahli.youssef@outlook.com>
2020-09-15 14:18:30 +0100

    export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-"""Using what we did in the task #0, extend Python script to export data in the CSV format."""
+"""
+Using what we did in the task #0, extend Python script
+to export data in the CSV format.
+"""
 
+import csv
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-	EMPLOYEE_NAME = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])).json().get('name')
-	req_todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    userId = argv[1]
+    if userId.isdigit():
+        req_name = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                                .format(userId))
+        name = req_name.json().get("name")
 
-	with open('{}.csv'.format(argv[1]), 'w+') as file:
-			for task in req_todo:
-				if task.get('userId') == int(argv[1]):
-					file.write('"{}","{}","{}","{}"\n'.format(task.get('userId'), EMPLOYEE_NAME, task.get('completed'), task.get('title')))
+        req_todo = requests.get("http://jsonplaceholder.typicode.com/" +
+                                "users/{}/todos".format(userId))
+        todo = req_todo.json()
+
+        with open('{}.csv'.format(userId), 'w') as file:
+            writer = csv.writer(file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL, lineterminator='\n')
+            for task in todo:
+                writer.writerow([userId, name, str(task.get('completed')),
+                                task.get('title')])
