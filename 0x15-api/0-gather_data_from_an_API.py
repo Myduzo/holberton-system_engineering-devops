a6be289ed08ae8c78982819b5ce7fa7ae2d89134
Myduzo <sahli.youssef@outlook.com>
2020-09-15 11:05:52 +0100

    list todo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,16 +2,16 @@
 """script that returns information about his/her TODO list progress."""
 
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
     req_todo = requests.get("http://jsonplaceholder.typicode.com/" +
-                            "users/{}/todos".format(argv[1]))
+                            "users/{}/todos".format(sys.argv[1]))
     todo = req_todo.json()
 
     req_name = requests.get("http://jsonplaceholder.typicode.com/" +
-                            "users/{}".format(argv[1]))
+                            "users/{}".format(sys.argv[1]))
     name = req_name.json()["name"]
 
     tasks_done = []
