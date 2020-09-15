1a30d0ec901c54cc80fce44a86c22e15d4638c21
Myduzo <sahli.youssef@outlook.com>
2020-09-15 11:15:29 +0100

    get fixed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,22 +2,22 @@
 """script that returns information about his/her TODO list progress."""
 
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
     req_todo = requests.get("http://jsonplaceholder.typicode.com/" +
-                            "users/{}/todos".format(sys.argv[1]))
+                            "users/{}/todos".format(argv[1]))
     todo = req_todo.json()
 
     req_name = requests.get("http://jsonplaceholder.typicode.com/" +
-                            "users/{}".format(sys.argv[1]))
-    name = req_name.json().get("name")
+                            "users/{}".format(argv[1]))
+    name = req_name.json()["name"]
 
     tasks_done = []
     for task in todo:
-        if task.get("completed") is True:
-            tasks_done.append(task.get("title"))
+        if task["completed"] is True:
+            tasks_done.append(task["title"])
     print("Employee {} is done with ".format(name) +
           "tasks({}/{}):".format(len(tasks_done), len(todo)))
 
