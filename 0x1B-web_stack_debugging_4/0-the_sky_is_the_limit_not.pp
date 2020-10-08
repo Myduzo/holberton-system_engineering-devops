19cb1c5c2a9f92411cb0479309adc1166e680426
Myduzo <sahli.youssef@outlook.com>
2020-10-08 22:48:12 +0100

    limit requests

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 
 exec { 'set limit to 2000 and restart nginx':
   path    => '/bin',
-  command => "sed -i 's/15/2000/' /etc/default/nginx && sudo service nginx restart"
+  command => 'sed -i "s/15/2000/" /etc/default/nginx && sudo service nginx restart'
 }
